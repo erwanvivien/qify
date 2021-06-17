19d3277e51feae45ee8318ab65b542b57e056367
erwan.vivien <erwan.vivien@epita.fr>
2021-06-17 21:51:56 +0200

    :building_construction: Moved controller to Room

--- src/Room.js
+++ src/Room.js
@@ -135,4 +135,68 @@ class Room {
   }
 }
 
-module.exports = Room;
+function createRoom(admin, spotify_cred, socket) {
+  let room = Room.getRoomWithAdmin(admin.id);
+  if (room) return socket.emit("RES_CREATE_ROOM", room.pin);
+
+  let pin = Room.createRoom(admin, spotify_cred, socket.id);
+  socket.emit("RES_CREATE_ROOM", pin);
+}
+
+function addSong(pin, song, io) {
+  let room = Room.getRoomWithPin(pin);
+  room.songQueue.push(song);
+  io.to(pin).emit("RES_ADD_SONG", room.songQueue);
+}
+
+function getSongs(pin, socket) {
+  let room = Room.getRoomWithPin(pin);
+  if (!room) return socket.emit("RES_GET_SONGS", null);
+
+  return socket.emit("RES_GET_SONGS", room.songQueue);
+}
+
+function checkRoom(pin, socket) {
+  let room = Room.getRoomWithPin(pin);
+  if (!room) return socket.emit("RES_CHECK_ROOM", null);
+
+  let roomLessInfo = {
+    pin: room.pin,
+    adminSpotifyId: room.adminSpotifyId,
+    access_token: room.spotify.access_token,
+    country: room.country,
+    songQueue: room.songQueue,
+  };
+  socket.emit("RES_CHECK_ROOM", roomLessInfo);
+}
+
+function joinRoom(pin, socket) {
+  let room = Room.getRoomWithPin(pin);
+  if (!room) return;
+
+  console.log(socket.id + " joined " + pin);
+  socket.join(pin);
+
+  room.members.push(socket.id);
+}
+
+function leaveRoom(pin, socket) {
+  let room = Room.getRoomWithPin(pin);
+  if (!room) return;
+
+  console.log(socket.id + " left " + pin);
+  socket.leave(pin);
+
+  let idx = room.members.indexOf(socket.id);
+  if (idx >= 0) room.members.splice(idx, 1);
+}
+
+module.exports = {
+  createRoom,
+  addSong,
+  getSongs,
+  checkRoom,
+  joinRoom,
+  leaveRoom,
+  Room,
+};
--- src/config.js
+++ src/config.js
@@ -69,7 +69,6 @@ module.exports = {
   authTokenEndpoint,
   clientId,
   clientSecret,
-  encodedClient,
   redirectUri,
   scopes,
   endpoints,
