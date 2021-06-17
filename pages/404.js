ef4cf230501414e4fba691054340595bf973c8d5
erwan.vivien <erwan.vivien@epita.fr>
2021-06-17 08:43:26 +0200

    :lipstick: Added logo to 404 & special text

--- pages/404.js
+++ pages/404.js
@@ -1,61 +1,69 @@
 // pages/404.js
 import Error from "next/error";
+import Head from "next/head";
 import { Component } from "react";
 
 class Custom404 extends Component {
   render() {
     // return <Error statusCode={404}></Error>;
     return (
-      <div
-        style={{
-          color: "#000",
-          fontFamily:
-            "-apple-system, BlinkMacSystemFont, Roboto, Segoe UI, Fira Sans, Avenir, Helvetica Neue, Lucida Grande, sans-serif",
-          height: "100vh",
-          justifyContent: "center",
-          display: "flex",
-          textAlign: "center",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <div>
-          <h1
-            style={{
-              display: "inline-block",
-              borderRight: "1px solid rgba(0, 0, 0,.3)",
-              margin: "0px 20px 0px 0px",
-              padding: "10px 23px 10px 0",
-              fontSize: "24px",
-              fontWeight: "500",
-              verticalAlign: "top",
-            }}
-          >
-            404
-          </h1>
-          <div
-            style={{
-              display: "inline-block",
-              textAlign: "left",
-              lineHeight: "49px",
-              height: "49px",
-              verticalAlign: "middle",
-            }}
-          >
-            <h2
+      <>
+        <Head>
+          <title>Page non-existante</title>
+          <link rel="icon" href="/partify/partify.svg" />
+        </Head>
+
+        <div
+          style={{
+            color: "#000",
+            fontFamily:
+              "-apple-system, BlinkMacSystemFont, Roboto, Segoe UI, Fira Sans, Avenir, Helvetica Neue, Lucida Grande, sans-serif",
+            height: "100vh",
+            justifyContent: "center",
+            display: "flex",
+            textAlign: "center",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <h1
               style={{
-                fontSize: "14px",
-                fontWeight: "normal",
-                lineHeight: "inherit",
-                margin: "0px",
-                padding: "0px",
+                display: "inline-block",
+                borderRight: "1px solid rgba(0, 0, 0,.3)",
+                margin: "0px 20px 0px 0px",
+                padding: "10px 23px 10px 0",
+                fontSize: "24px",
+                fontWeight: "500",
+                verticalAlign: "top",
               }}
             >
-              La page n'existe pas
-            </h2>
+              404
+            </h1>
+            <div
+              style={{
+                display: "inline-block",
+                textAlign: "left",
+                lineHeight: "49px",
+                height: "49px",
+                verticalAlign: "middle",
+              }}
+            >
+              <h2
+                style={{
+                  fontSize: "14px",
+                  fontWeight: "normal",
+                  lineHeight: "inherit",
+                  margin: "0px",
+                  padding: "0px",
+                }}
+              >
+                La page n'existe pas
+              </h2>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
