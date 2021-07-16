c7600607a117db08bc2049068a5ba47aee8fdaf8
erwan.vivien <erwan.vivien@epita.fr>
2021-07-16 11:47:53 +0200

    :rocket: Using PWA to make the application mobile

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="fr">
-        <Head />
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
         <body>
           <Main />
           <NextScript />
