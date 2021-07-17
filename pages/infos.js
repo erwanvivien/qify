7a56f69a3721f406cb56f477cb458d8e143cbd25
erwan.vivien <erwan.vivien@epita.fr>
2021-07-17 16:34:31 +0200

    :rotating_light: Escaped

--- pages/infos.js
+++ pages/infos.js
@@ -30,7 +30,7 @@ class About extends Component {
               </ul>
             </li>
             <li>
-              <a href="#who">Qui est à l'origine de Qify ?</a>
+              <a href="#who">Qui est à l&apos;origine de Qify ?</a>
             </li>
             <li>
               <a href="#who_to_contact">Qui contacter en cas de problème ?</a>
@@ -46,31 +46,31 @@ class About extends Component {
             Comment utiliser Qify ?
           </h2>
           <p>
-            Qify est un logiciel complètement gratuit d'usage. Qify utilise
+            Qify est un logiciel complètement gratuit d&apos;usage. Qify utilise
             votre compte Spotify en ajoutant des musiques dans la file
-            d'attente. Cela permet à tout le monde de pouvoir participer tout en
-            s'assurant que personne n'ai accès à votre compte Spotify ou votre
-            appareil
+            d&apos;attente. Cela permet à tout le monde de pouvoir participer
+            tout en s&apos;assurant que personne n&apos;ai accès à votre compte
+            Spotify ou votre appareil
           </p>
           <p>
             <b>Rejoindre un salon</b>:
-            <br />- Scanner le QR code, disponible sur la page de l'admin
-            <br />- Utiliser le lien que l'admin peut fournir en cliquant sur le
-            QR code
+            <br />- Scanner le QR code, disponible sur la page de l&apos;admin
+            <br />- Utiliser le lien que l&apos;admin peut fournir en cliquant
+            sur le QR code
             <br />- Cliquer sur le{" "}
             <a className={style_infos.links} href={paths.join}>
               bouton rejoindre
             </a>{" "}
-            sur la page d'accueil et de rentrer le code du salon
+            sur la page d&apos;accueil et de rentrer le code du salon
           </p>
           <p>
-            <b>Créer un salon / Déjà admin d'un salon</b>
+            <b>Créer un salon / Déjà admin d&apos;un salon</b>
             <br />- Cliquer sur le{" "}
             <a className={style_infos.links} href={paths.create}>
               bouton créer
             </a>{" "}
-            sur la page d'accueil et de se connecter à Spotify. Tout le reste de
-            la procédure est fait automatiquement.
+            sur la page d&apos;accueil et de se connecter à Spotify. Tout le
+            reste de la procédure est fait automatiquement.
           </p>
           <p>
             <b>Ajouter des musiques</b>
@@ -83,38 +83,41 @@ class About extends Component {
               <u>Apple</u>
             </h3>
             <p>
-              Pour le moment il n'y a pas de moyen pour un appareil Apple
-              d'utiliser Qify en tant qu'admin d'un salon.
+              Pour le moment il n&apos;y a pas de moyen pour un appareil Apple
+              d&apos;utiliser Qify en tant qu&apos;admin d&apos;un salon.
               <br />
-              Cependant, il est possible de télécharger l'application en allant
-              dans les paramètres de la page et en cliquant sur Ajouter à la
-              page d'accueil
+              Cependant, il est possible de télécharger l&apos;application en
+              allant dans les paramètres de la page et en cliquant sur Ajouter à
+              la page d&apos;accueil
             </p>
 
             <h3 id="how_to_use_android">
               <u>Android</u>
             </h3>
             <p>
-              Télécharger l'application directement sur le playstore ou de
-              rajouter l'application à votre écran d'accueil grâce au bouton sur
-              la page d'accueil.
+              Télécharger l&apos;application directement sur le playstore ou de
+              rajouter l&apos;application à votre écran d&apos;accueil grâce au
+              bouton sur la page d&apos;accueil.
             </p>
 
             <h3 id="how_to_use_windows">
               <u>Windows</u>
             </h3>
             <p>
-              Télécharger l'application grâce au bouton sur la page d'accueil
+              Télécharger l&apos;application grâce au bouton sur la page
+              d&apos;accueil
             </p>
 
             <h3 id="how_to_use_television">
               <u>Télévision</u>
             </h3>
-            <p>Pour le moment, il n'y a pas de moyen pour un appareil télé</p>
+            <p>
+              Pour le moment, il n&apos;y a pas de moyen pour un appareil télé
+            </p>
           </div>
 
           <h2 id="who" className={style_infos.h2_decorator}>
-            Qui est à l'origine de Qify ?
+            Qui est à l&apos;origine de Qify ?
           </h2>
           <p>
             Mon nom est <b>Erwan VIVIEN</b>, je suis un developpeur Français de{" "}
@@ -122,8 +125,8 @@ class About extends Component {
               (new Date().getTime() - new Date("2000-09-14").getTime()) /
                 (1000 * 60 * 60 * 24 * 365.25)
             )}{" "}
-            ans, à l'origine de l'idée et de la réalisation du projet. Vous
-            pouvez me contacter sur différents médias (accessibles sur la{" "}
+            ans, à l&apos;origine de l&apos;idée et de la réalisation du projet.
+            Vous pouvez me contacter sur différents médias (accessibles sur la{" "}
             <a className={style_infos.links} href={paths.contact}>
               page contact
             </a>
@@ -146,11 +149,11 @@ class About extends Component {
             Où faire remonter les bugs ?
           </h2>
           <p>
-            Tout d'abord merci, venir reporter un bug est un immense privilège.
-            Si vous avez quelconque idée de la manière pour le reproduire
-            (appuyer rapidement sur un bouton, mettre 3 musiques d'un coup avec
-            des amis, etc...) je serai ravi d'essayer de reproduire ce bug et
-            donc de le corriger ensuite.
+            Tout d&apos;abord merci, venir reporter un bug est un immense
+            privilège. Si vous avez quelconque idée de la manière pour le
+            reproduire (appuyer rapidement sur un bouton, mettre 3 musiques
+            d&apos;un coup avec des amis, etc...) je serai ravi d&apos;essayer
+            de reproduire ce bug et donc de le corriger ensuite.
             <br />
             Vous pouvez donc faire remonter les bugs sur cette page :{" "}
             <a
@@ -159,6 +162,7 @@ class About extends Component {
             >
               Faire remonter les bugs
             </a>
+            .
           </p>
         </div>
       </Default>
