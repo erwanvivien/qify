3a06f6038d84f785d78b1159bbb00a7127ac029b
erwan.vivien <erwan.vivien@epita.fr>
2021-07-17 09:44:02 +0200

    :sparkles: using react component instead of function

--- pages/index.js
+++ pages/index.js
@@ -6,39 +6,44 @@ import { paths } from "../src/config";
 
 import spotifyToggle from "../public/spotify-toggle.png";
 import Image from "next/image";
+import { Component } from "react";
 
-export default function Home() {
-  return (
-    <>
-      <Default footer={false}>
-        <div className={styles.grid}>
-          <a href={paths.create} className={styles.card}>
-            <h2 style={{ textAlign: "center", margin: "0" }}>
-              Créer un salon<br></br>Rejoindre en admin
-            </h2>
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <Default footer={false}>
+          <div className={styles.grid}>
+            <a href={paths.create} className={styles.card}>
+              <h2 style={{ textAlign: "center", margin: "0" }}>
+                Créer un salon<br></br>Rejoindre en admin
+              </h2>
 
-            <Image
-              src={spotifyToggle}
-              alt="Spotify toggle to show that you're using your spotify account"
-            />
-          </a>
+              <Image
+                src={spotifyToggle}
+                alt="Spotify toggle to show that you're using your spotify account"
+              />
+            </a>
 
-          <a href={paths.join} className={styles.card}>
-            <h2 style={{ textAlign: "center", margin: "0" }}>
-              Rejoindre un salon
-            </h2>
-            <div
-              style={{
-                height: "100%",
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <p>Rentrer le code ou utiliser le code QR</p>
-            </div>
-          </a>
-        </div>
-      </Default>
-    </>
-  );
+            <a href={paths.join} className={styles.card}>
+              <h2 style={{ textAlign: "center", margin: "0" }}>
+                Rejoindre un salon
+              </h2>
+              <div
+                style={{
+                  height: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <p>Rentrer le code ou utiliser le code QR</p>
+              </div>
+            </a>
+          </div>
+        </Default>
+      </>
+    );
+  }
 }
+
+export default Home;
