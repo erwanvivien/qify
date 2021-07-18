50a26538d3e6fca15b6aa7c3cee8ce86577b92ad
erwan.vivien <erwan.vivien@epita.fr>
2021-07-18 18:41:22 +0200

    :wrench: Can use on local lan

--- src/server.js
+++ src/server.js
@@ -85,7 +85,7 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(port, (err) => {
+  server.listen(port, "0.0.0.0", (err) => {
     if (err) throw err;
     console.log(`> Ready on ${currentUrl}`);
   });
